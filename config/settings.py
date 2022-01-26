56e82b1cbd6954a980ba368b4b85183a41e092ae
Patrick <pat_pi@yahoo.com>
2022-01-26 20:45:21 +0100

    custom user

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
 
     # Local
-
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -149,6 +149,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Allauth
 SITE_ID = 1
